505a549d72732e5305892d65a60748f66efde6a3
Shreyas <sspansare95@gmail.com>
2015-12-10 23:27:06 +0530

    Initial commit

---
+++ server/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask import request
+import requests
+import json
+import ast
+import temperature
+import requests
+sm = 0
+t = {}
+app = Flask(__name__)
+
+                
+if __name__ == '__main__':
+	app.run(host="192.168.0.105")
+
