ac639425c4c1d98bdfd229cbc4ba51be8cfcfd07
niranjan09 <niranjanketkar99@gmail.com>
2015-12-18 17:39:06 +0530

    This is commit by niranjan

--- android/NewDemo/app/src/main/java/com/example/shreyas/newdemo/MainActivity.java
+++ android/NewDemo/app/src/main/java/com/example/shreyas/newdemo/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.shreyas.newdemo;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
 
 
+    private Toolbar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Team END!!!");
+
+
+        Navigation_Drawer drawerfragment=(Navigation_Drawer)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerfragment.setUP((DrawerLayout) findViewById(R.id.drawer_layout_id), toolbar);
+
+
     }
+
+
 }
---
+++ android/NewDemo/app/src/main/java/com/example/shreyas/newdemo/Navigation_Drawer.java
@@ -0,0 +1,88 @@
+package com.example.shreyas.newdemo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Navigation_Drawer extends android.support.v4.app.Fragment
+{
+    private ActionBarDrawerToggle nDrawerToggle;
+    private DrawerLayout nDrawerLayout;
+
+    public Navigation_Drawer()
+    {
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null)
+        {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View view=inflater.inflate(R.layout.fragment_navigation__drawer, container, false);
+
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+    }
+    public void setUP(DrawerLayout drawerlayout,Toolbar toolbar) {
+        nDrawerLayout = drawerlayout;
+        nDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerlayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+
+            public void onDrawerOpened(View drawerview) {
+                super.onDrawerOpened(drawerview);
+//                signin_btn = (Button) getView().findViewById(R.id.signinbutton);
+//                signin_btn.setText("Sign Out");
+
+
+
+            }
+            public void onDrawerClosed(View drawerview)
+            {
+                super.onDrawerClosed(drawerview);
+            }
+        };
+        nDrawerLayout.setDrawerListener(nDrawerToggle);
+
+        nDrawerLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                nDrawerToggle.syncState();
+            }
+        });
+    }
+
+
+
+}
