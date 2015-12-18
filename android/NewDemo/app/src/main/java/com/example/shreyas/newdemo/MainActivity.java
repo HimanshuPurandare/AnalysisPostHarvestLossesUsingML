2be12c3d62846544ddf28ffdbe2b99cf5db84139
Shreyas <sspansare95@gmail.com>
2015-12-18 19:41:02 +0530

    First app

--- android/NewDemo/app/src/main/java/com/example/shreyas/newdemo/MainActivity.java
+++ android/NewDemo/app/src/main/java/com/example/shreyas/newdemo/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.shreyas.newdemo;
 
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity
 {
     private Toolbar toolbar;
+    private Navigation_Drawer drawerfragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,18 +16,27 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupToolbar();
+
+        setupNavigationDrawer();
+
+
+    }
+
+    void setupToolbar()
+    {
         toolbar = (Toolbar) findViewById(R.id.mytoolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Team END!!!");
         getSupportActionBar().setSubtitle("Pune");
         toolbar.setTitleTextColor(getResources().getColor(R.color.Text_Icon));
         toolbar.setSubtitleTextColor(getResources().getColor(R.color.LightPrimaryColor));
-
-        Navigation_Drawer drawerfragment=(Navigation_Drawer)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerfragment.setUP((DrawerLayout) findViewById(R.id.drawer_layout_id), toolbar);
-
-
     }
 
+    void setupNavigationDrawer()
+    {
+        drawerfragment=(Navigation_Drawer)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerfragment.setUP((DrawerLayout) findViewById(R.id.drawer_layout_id), toolbar);
+    }
 
 }
