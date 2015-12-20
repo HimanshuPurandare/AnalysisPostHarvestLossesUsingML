8dc34ad5057e07efc391cf31f880f4e9b88d8ce5
Shreyas <sspansare95@gmail.com>
2015-12-20 12:03:28 +0530

    Expandable listview

--- android/NewDemo/app/src/main/java/com/example/shreyas/newdemo/MyFarm.java
+++ android/NewDemo/app/src/main/java/com/example/shreyas/newdemo/MyFarm.java
@@ -4,10 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MyFarm extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List expandableListTitle;
+    HashMap expandableListDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,48 @@ public class MyFarm extends AppCompatActivity {
         String farmname = getIntent().getStringExtra("FarmName");
 
         Log.d("FarmName",farmname);
+
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        expandableListDetail = ExpandableListDataPump.getData();
+        expandableListTitle = new ArrayList(expandableListDetail.keySet());
+        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)), Toast.LENGTH_SHORT
+                )
+                        .show();
+                return false;
+            }
+        });
 
         setupToolbar(farmname);
     }
