49fea9e34819c1aa3f328d421377761f06e511c7
Shreyas <sspansare95@gmail.com>
2016-01-13 12:12:32 +0530

    avghad e aata

--- arduino/arduino_code/arduino_code.ino
+++ arduino/arduino_code/arduino_code.ino
@@ -7,15 +7,15 @@ Serial.begin(9600);
 }
 
 void loop() {
-Serial.print("\n");
 delay(100);
 Serial.print(analogRead(A1)*100/1024);
 delay(100);
 int humidity = DHT.read11(A2);
-Serial.print("\t");
+Serial.print("\n");
 Serial.print(DHT.humidity);
 delay(100);
-Serial.print("\t");
+Serial.print("\n");
 Serial.print(DHT.temperature);
+Serial.print("\n");
 delay(700);
 }
